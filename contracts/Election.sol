09132b83c26a4b265db9e870b553b18c3414854e
jamesgeorge007 <jamesgeorge998001@gmail.com>
2018-08-27 12:21:42 +0530

    Add candidate info to the smart contract :rocket:

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -1,9 +1,24 @@
 pragma solidity^0.4.20;
 
-contract Election {
-    string public candidate; 
 
-    function Election () public {
-        candidate = "Candidate-1";
+contract Election {
+    struct Candidate {
+        uint id;
+        string name;
+        uint voteCount;
+    }
+
+    mapping(uint => Candidate) public candidates;
+
+    uint public candidatesCount;
+
+    constructor() public {
+        addCandidate("Candidate-1");
+        addCandidate("Candidate-2");
+    }
+
+    function addCandidate(string _name) private{
+        candidatesCount ++;
+        candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
     }
 }
